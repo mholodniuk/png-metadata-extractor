7015423fab0cc1e1c724ab10587748bf3decf4c1
maciejholodniuk <maciekholo@gmail.com>
2023-05-20 17:20:09 +0200

    refactor + add information about number of entries in PLTE chunk

--- api/src/main/java/com/dev/imageprocessingapi/metadataextractor/model/RawChunk.java
+++ api/src/main/java/com/dev/imageprocessingapi/metadataextractor/model/RawChunk.java
@@ -1,11 +1,6 @@
 package com.dev.imageprocessingapi.metadataextractor.model;
 
-public record RawChunk(String type,
-                       int length,
-                       int offset,
-                       byte[] rawBytes,
-                       String CRC
-                    ) {
+public record RawChunk(String type, int length, int offset, byte[] rawBytes, String CRC) {
     public static final int LENGTH_FIELD_LENGTH = 4;
     public static final int TYPE_FIELD_LENGTH = 4;
     public static final int CRC_FIELD_LENGTH = 4;
